2bfb77f339173774e5cff368fa45d890589f6a14
Roan Atkinson <roanatkinson209@gmail.com>
2018-01-28 07:01:35 +1100

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cold Waters Vessel Data
+
+###### by Roan Atkinson
+
+<br>
+New Playable Subs & More Updater is a semi-auto updater for the <a href="https://github.com/CaptainX3/CW-Playable-Subs/">Playable Subs & More Mod</a>, a mod the the game <a href="http://store.steampowered.com/app/541210/Cold_Waters/">Cold Waters</a>. New Playable Subs & More Updater is designed so you can update your mod at the click of a button, without having to re-download the entire mod. If you have any issues please open an issue on the GitHub page and I will see to it as soon as possible.
+
+#### How to install
+See that bright green button on the right hand side of the page? The one that says "Clone or download". Click that, then choose "Download ZIP". Once downloaded open the folder and double click on "installer.bat". This will guide you through installation and will automatically install the New Playable Subs & More Updater for you.
+
+#### Warning
+If you have an anti virus it might mark the program as suspicious, due to the fact that I have used batch (.bat) to develop this. You should be able to override it and execute the installer.bat
---
+++ installer.bat
@@ -0,0 +1,70 @@
+@echo off
+color 70
+title New Playable Subs Mod ^& More Updater - Installer
+cls
+
+echo Checking for Git...
+git --version >nul 2>&1
+if errorlevel 1 goto :getgit
+echo Git installed
+echo Making files...
+cd "%UserProfile%"
+rmdir .PBSmodUpdater /S /Q >nul 2>&1
+mkdir .PBSmodUpdater
+cd .PBSmodUpdater
+git clone https://github.com/CaptainX3/CW-Playable-Subs.git
+echo.
+:askPath
+echo Please paste the path to your Cold Waters game files folder
+echo If you're using Steam it will likely be: C:\Program Files (x86)\Steam\steamapps\common\Cold Waters
+echo If you're using GOG it will likely be: C:\GOG Games\Cold Waters
+set /p cwPath="Path: "
+cd "%cwPath%" >nul 2>&1
+if errorlevel 1 goto :invalidPath
+REM obscure path to verify the game folder
+cd "%cwPath%\ColdWaters_Data\StreamingAssets\default\audio\music" >nul 2>&1
+if errorlevel 1 goto :invalidPath
+cd "%UserProfile%\.PBSmodUpdater"
+echo.
+echo Making files...
+@echo %cwPath%\ColdWaters_Data\StreamingAssets\> path.conf
+@echo @echo off> updateMod.bat
+@echo color 70>> updateMod.bat
+@echo title New Playable Subs Mod ^^^& More Updater>> updateMod.bat
+@echo cls>> updateMod.bat
+@echo cd "%%UserProfile%%\.PBSmodUpdater">> updateMod.bat
+@echo cd "CW-Playable-Subs">> updateMod.bat
+@echo git pull>> updateMod.bat
+@echo cd ..>> updateMod.bat
+@echo set /p gameDir=^<path.conf>> updateMod.bat
+@echo cd "%%gameDir%%">>updateMod.bat
+@echo rmdir override /S /Q>>updateMod.bat
+@echo mkdir override>>updateMod.bat
+@echo xcopy "%UserProfile%\.PBSmodUpdater\CW-Playable-Subs\override" .\override /E>>updateMod.bat
+@echo echo Mod Updated.>>updateMod.bat
+@echo pause>>updateMod.bat
+@echo exit>>updateMod.bat
+copy updateMod.bat %UserProfile%\Desktop
+echo.
+echo Installation complete
+echo The updater has been copied to your desktop.
+echo Just doubleclick on the "updateMod.bat" to update the mod
+echo If you wish to uninstall, navigate to %UserProfile% and delete the .PBSmodUpdater folder
+echo Enjoy :^)
+pause
+exit
+
+:getgit
+echo.
+echo Git not installed, please install Git for windows, then restart the installation:
+echo http://gitforwindows.org/
+pause
+exit
+
+:invalidPath
+echo.
+echo Invalid path
+echo Are you sure it's correct?
+pause
+echo.
+goto :askPath
